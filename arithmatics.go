738cc0b800a8a16ff6171644c8387c1e895addb1
piyush175 <piyush1751995@gmail.com>
2022-08-15 19:32:19 +0530

    waba feet

--- arithmatics.go
+++ arithmatics.go
@@ -1,10 +1,18 @@
 package arithmatics
 
+import (
+	"fmt"
+)
+
 func IsPrime(num int) bool {
-    for i := 2; i < int(num/2); i++ {
-        if num%i == 0 {
-            return false
-        }
-    }
-    return true
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func WabaFeet() {
+	fmt.Println("Wa waba feet!!!!!!!!!!!!!!!")
 }
